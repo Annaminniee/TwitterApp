8ccf29fd293d5b0cbd2cac24a18b21a61b43001d
Anna <anna.kikuchihara@gmail.com>
2023-07-24 09:40:22 +0900

    指摘3のoutletの接続をしました

--- TwitterApp/HomeViewController.swift
+++ TwitterApp/HomeViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         navigationItem.title = "おすすめ"
         print("HomeViewControllerが表示されました！")
