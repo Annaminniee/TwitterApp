2396383962c841e1b55a1f7556cc7f71cf05b2fe
Anna <anna.kikuchihara@gmail.com>
2023-07-26 11:17:10 +0900

    UITableViewにダミーデータを表示しました

--- TwitterApp/HomeViewController.swift
+++ TwitterApp/HomeViewController.swift
@@ -11,20 +11,36 @@ class HomeViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    var tweetDataList: [TweetDataModel] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "おすすめ"
         print("HomeViewControllerが表示されました！")
+        
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        setTweetData()
+    }
+    
+    func setTweetData() {
+        for i in 1...3 {
+            let tweetDataModel = TweetDataModel(text: "ツイート内容")
+            tweetDataList.append(tweetDataModel)
+        }
     }
 }
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     /// データの数（＝セルの数）を返すメソッド
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return tweetDataList.count
     }
     /// 各セルの内容を返すメソッド
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = UITableViewCell()
+        let tweetDataModel: TweetDataModel = tweetDataList[indexPath.row]
+        cell.textLabel?.text = tweetDataModel.text
+        return cell
     }
 }
---
+++ TwitterApp/TweetDataModel.swift
@@ -0,0 +1,12 @@
+//
+//  TweetDataModel.swift
+//  TwitterApp
+//
+//  Created by 菊地原杏菜 on 2023/07/26.
+//
+
+import Foundation
+
+struct TweetDataModel {
+    var text: String
+}
