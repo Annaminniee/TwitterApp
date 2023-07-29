fc8784c20a5e7967bc144e0b41cf42ea6a21b2e4
Anna <anna.kikuchihara@gmail.com>
2023-07-29 15:58:43 +0900

    nameを追加しました

---
+++ TwitterApp/UserNameDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserNameDataModel.swift
+//  TwitterApp
+//
+//  Created by 菊地原杏菜 on 2023/07/29.
+//
+
+import Foundation
+
+struct UserNameDataModel {
+    var text: String
+    var name: String
+}
