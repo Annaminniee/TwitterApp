7718136e9ea8ff577934a3f71d80984f012ef5c2
Anna <anna.kikuchihara@gmail.com>
2023-08-11 09:28:46 +0900

    キャンセルとツイートボタンの実装を修正しました

--- TwitterApp/EditorViewController.swift
+++ TwitterApp/EditorViewController.swift
@@ -19,33 +19,52 @@ class EditorViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         displayData()
-        configureTweetButton()
         setDoneButton()
-        setNavigationBarButton()
+        configureCancelButtonItem()
+        configureBarButtonItems()
+    }
+    
+    /// キャンセルボタンの設定
+    func configureCancelButtonItem() {
+        let closeButton = UIBarButtonItem(title: "キャンセル",
+                                          style: .plain,
+                                          target: self,
+                                          action: #selector(closeButtonTapped))
+        navigationItem.leftBarButtonItem = closeButton
+    }
+    
+    @objc func closeButtonTapped() {
+        // 前の画面に戻る
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // ツイートボタンの設定
+    func createCustomButton() -> UIButton {
+        let customButton = UIButton(type: .custom)
+        customButton.setTitle("ツイートする", for: .normal)
+        customButton.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+        customButton.backgroundColor = UIColor.systemBlue
+        customButton.layer.cornerRadius = 16
+        customButton.addTarget(self,
+                               action: #selector(tweetButtonTapped),
+                               for: .touchUpInside)
+        customButton.frame = CGRect(x: 0, y: 0, width: 115, height: 32)
+        return customButton
+    }
+    
+    func configureBarButtonItems() {
+        let barButtonItem = UIBarButtonItem(customView: createCustomButton())
+        navigationItem.rightBarButtonItem = barButtonItem
+    }
+    
+    @objc func tweetButtonTapped() {
+        // ボタンがタップされたときの処理をここに記述
+        // TODO: 保存して閉じる処理
     }
     
     func displayData() {
         userName.text = "ユーザー名"
         textView.text = text
-    }
-    
-    func configureTweetButton() {
-        tweetButton.layer.cornerRadius = 20.0
-    }
-    
-    // 閉じるボタンをタップ
-    @objc func tapcancelButton() {
-        // 前の画面に戻る
-        dismiss(animated: true, completion: nil)
-    }
-    
-    @objc func taptweetButton() {
-        // TODO: 保存して閉じる処理
-    }
-    
-    func setNavigationBarButton() {
-        let _: Selector = #selector(tapcancelButton)
-        let _: Selector = #selector(taptweetButton)
     }
     
     func transitionToHomeView() {
