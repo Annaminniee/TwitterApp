bd2f54db5e34c3e5a6ff5c4b192fd05100517afc
Anna <anna.kikuchihara@gmail.com>
2023-08-22 10:04:29 +0900

    テストコードを実装しました

--- TwitterAppTests/TwitterAppTests.swift
+++ TwitterAppTests/TwitterAppTests.swift
@@ -18,12 +18,12 @@ final class TwitterAppTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testTextView() throws {
+        let vc = EditorViewController()
+        let number = 140
+        let isActive = true
+        XCTAssertEqual(number, 140)
+        XCTAssertTrue(isActive)
     }
 
     func testPerformanceExample() throws {
